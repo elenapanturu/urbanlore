1817fa4955d076552ebf6a91f7cbb5af666770bc
Elena Panturu <elenapanturu29@gmail.com>
2025-05-13 17:43:00 +0300

    Fixing deployment issues

--- utils/functions.js
+++ utils/functions.js
@@ -1,6 +1,16 @@
-import {connectToDatabase} from '../lib/mongodb';
+import { connectToDatabase } from '../lib/mongodb';
 
 export const getCollection = async collectionName => {
-	const {database,} = await connectToDatabase();
-	return database.collection(collectionName);
+	try {
+		const { database, } = await connectToDatabase();
+		if (!database) {
+			throw new Error('Database connection failed');
+		}
+
+		return database.collection(collectionName);
+	} catch (error) {
+		console.error('Error in getCollection:', error);
+		throw new Error('Failed to get collection');
+	}
+
 };
